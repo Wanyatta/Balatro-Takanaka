7e1ec6effbc91b92acf7a19d1df1529dd00ed4ea
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-09-08 16:45:42 +0200

    file restructuring with slowed music config

---
+++ src/altas.lua
@@ -0,0 +1,16 @@
+SMODS.Atlas {
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+}
+
+SMODS.Atlas {
+    key = "balatro", 
+    path = "balatro.png", 
+    px = 333,
+    py = 216,
+    prefix_config = { key = false },
+    atlas_table = "ASSET_ATLAS"
+}
