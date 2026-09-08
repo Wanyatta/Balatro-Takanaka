1cef134f3dd81fcfb7c330c68b6c036d4035b24a
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-09-08 17:06:47 +0200

    cleanup magic numbers in music.lua

--- src/music.lua
+++ src/music.lua
@@ -1,48 +1,48 @@
-local music_directory = ""
+local music_directory = "original/"
+local music_pitch = 1.0
+local music_volume = 0.3
 local config = SMODS.current_mod.config
 
-if not config.music_slowed then
-    music_directory = "original/"
-else
+if music_slowed then
     music_directory = "slowed/"
 end
 
 SMODS.Sound({
     key = "music1",
-    pitch = 1.0,
-    volume = 0.3,
+    pitch = music_pitch,
+    volume = music_volume,
     path = music_directory .. "takanaka_main.ogg",
     replace = "music1"
 })
 
 SMODS.Sound({
     key = "music2",
-    pitch = 1.0,
-    volume = 0.3,
+    pitch = music_pitch,
+    volume = music_volume,
     path = music_directory .. "takanaka_arcana.ogg",
     replace = "music2"
 })
 
 SMODS.Sound({
     key = "music3",
-    pitch = 1.0,
-    volume = 0.3,
+    pitch = music_pitch,
+    volume = music_volume,
     path = music_directory .. "takanaka_celestial.ogg",
     replace = "music3"
 })
 
 SMODS.Sound({
     key = "music4",
-    pitch = 1.0,
-    volume = 0.3,
+    pitch = music_pitch,
+    volume = music_volume,
     path = music_directory .. "takanaka_shop.ogg",
     replace = "music4"
 })
 
 SMODS.Sound({
     key = "music5",
-    pitch = 1.0,
-    volume = 0.3,
+    pitch = music_pitch,
+    volume = music_volume,
     path = music_directory .. "takanaka_boss.ogg",
     replace = "music5"
 })
